af945a9c113193b07f08dda4a599696d21d6ee7c
ilas-pakaya <ilaspakaya@ilas-2.local>
2025-03-13 11:57:02 +0700

    menampilkan array dan jumlah dari pass

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,4 +1,32 @@
-git
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[];          //deklarasi variable global array a dengan ukuran 20
+int n;              // deklarasi variable dengan global n untuk menyimpan banyaknya elemen pada array
+
+void input(){       //procedur untuk input
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array : ";  //output ke layar
+        cin >> n;       //input dari pengguna
+        if (n <= 20)    //jika n kurang dari atau sama dengan 20
+            break;      //keluar dari loop
+        else {          //jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";   //output ke layar 
+        }
+
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i <n; i++){
+        cout << "Data ke-" << (i+1) << ";";
+        cin >> arr[i];
+    }
+}
+
 void bubbleSortArray()
 {
     int pass = 1; //step 1
@@ -20,4 +48,24 @@ void bubbleSortArray()
         pass = pass + 1; //step 4
 
     } while (pass <= n - 1); //step 5
+}
+
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j]; //menampilkan array
+        if (j < n - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
 }
