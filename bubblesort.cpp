547e74110579e60e2762808a69713db8c66647e2
ilas-pakaya <ilaspakaya@ilas-2.local>
2025-03-13 11:44:56 +0700

    first commit

---
+++ bubblesort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[];          //deklarasi variable global array a dengan ukuran 20
+int n;              // deklarasi variable dengan global n untuk menyimpan banyaknya elemen pada array
